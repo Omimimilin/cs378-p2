34d88266afffb8c9bb3b61cc07360960a65a2583
Naomi Lin <naomilin4668@gmail.com>
2025-02-21 08:57:23 -0600

    center

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -19,26 +19,26 @@ const MenuItem = ({ item, quantity, onQuantityChange }) => {
 
     return (
         <div className="container mb-4">
-            <div className="row justify-content-center">
-                <div className="col-4">
-                    <img className="foodpic img-fluid" src={`${process.env.PUBLIC_URL}/images/${imageName}`} alt={title} />
-                </div>
-                <div className="col-8">
-                    <div className="foodTitle h4">{title}</div>
-                    <div className="foodDescription">{description}</div>
-                    <div className="foodPrice">
-                        <div>
-                            <span className="h5">${price.toFixed(2)}</span>
-                            <div className="quantity-controls mt-2">
-                                <button className="btn btn-outline-secondary me-2" onClick={handleDecrement}>-</button>
-                                <span className="quantity">{quantity}</span>
-                                <button className="btn btn-outline-secondary ms-2" onClick={handleIncrement}>+</button>
-                            </div>
-                        </div>
+    <div className="row justify-content-center">
+        <div className="col-4 offset-1">
+            <img className="foodpic img-fluid" src={`${process.env.PUBLIC_URL}/images/${imageName}`} alt={title} />
+        </div>
+        <div className="col-6">
+            <div className="foodTitle h4">{title}</div>
+            <div className="foodDescription">{description}</div>
+            <div className="foodPrice">
+                <div>
+                    <span className="h5">${price.toFixed(2)}</span>
+                    <div className="quantity-controls mt-2">
+                        <button className="btn btn-outline-secondary me-2" onClick={handleDecrement}>-</button>
+                        <span className="quantity">{quantity}</span>
+                        <button className="btn btn-outline-secondary ms-2" onClick={handleIncrement}>+</button>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
+</div>
     );
 };
 
